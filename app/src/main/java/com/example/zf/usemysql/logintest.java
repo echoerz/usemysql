99ba60a9ba128b943f54548733aeffabaed7e8c9
Charlie <18810838733@163.com>
2018-06-09 22:11:20 +0800

    优化了注册界面

--- app/src/main/java/com/example/zf/usemysql/logintest.java
+++ app/src/main/java/com/example/zf/usemysql/logintest.java
@@ -233,6 +233,8 @@ public class logintest extends AppCompatActivity implements View.OnClickListener
         }
         initView();
 
+
+
         TextView zhuce = (TextView)findViewById(R.id.zhuce_t);
         zhuce.setOnClickListener(new View.OnClickListener() {
             @Override
