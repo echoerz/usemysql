9eb68a2f1bc3bc1c351324f4365833eadad6eda4
echoer.z <1755667423@qq.com>
2018-06-10 21:23:39 +0800

    整理文件夹 6.10.21.23

--- app/src/main/java/com/example/zf/usemysql/myroom/MainZhuYe.java
+++ app/src/main/java/com/example/zf/usemysql/myroom/MainZhuYe.java
@@ -1,5 +1,7 @@
 package com.example.zf.usemysql.myroom;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,11 +11,13 @@ import android.os.Build;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.zf.usemysql.R;
 
 public class MainZhuYe extends AppCompatActivity {
     ImageView ivBackGround;
+    private SharedPreferences pref2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +30,16 @@ public class MainZhuYe extends AppCompatActivity {
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
+        //TextView name1 = (TextView)findViewById(R.id.room_name);
+        pref2 = PreferenceManager.getDefaultSharedPreferences(this);
+        String user = pref2.getString("user","");
+        String sex = pref2.getString("sex","");
+        String ages = pref2.getString("ages","");
+        String location = pref2.getString("location","");
+        if (location.equals("")){
+            //修改主页信息
 
+        }
         Bitmap bitmap = BlurBitmapUtil.blurBitmap(this, BitmapFactory.decodeResource(getResources(), R.mipmap.touxiang), 3f);
         ivBackGround.setImageBitmap(bitmap);
     }
