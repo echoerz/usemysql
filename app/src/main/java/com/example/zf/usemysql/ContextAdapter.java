4482d196e00f8437b213549ba2c3d1f236937df3
Charlie <18810838733@163.com>
2018-06-22 16:12:01 +0800

    2

--- app/src/main/java/com/example/zf/usemysql/ContextAdapter.java
+++ app/src/main/java/com/example/zf/usemysql/ContextAdapter.java
@@ -2,8 +2,6 @@ package com.example.zf.usemysql;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
